dd00d4e88aa30960698bc83faa22961dc7ee0c17
Coderz75 <n.syed.dev@gmail.com>
2026-09-18 10:30:26 -0700

    fix selection dragging when dragging from sim to index.

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,6 @@
 "use strict";
 
 class Simulation{
-
     constructor(){
         this.svg = document.getElementById("sim-svg");
         this.init_events();
@@ -29,6 +28,7 @@ class Simulation{
         });
         this.svg.addEventListener("pointerup",(event)=>{
             this.isPointerDown = false;
+            this.svg.releasePointerCapture(event.pointerId); // Releases pointer focus
             this.viewBox.x = this.newViewBox.x;
             this.viewBox.y = this.newViewBox.y;
         });
@@ -36,29 +36,23 @@ class Simulation{
             if(!this.isPointerDown) return;
             event.preventDefault();
             var pointerPosition = this.getPointFromEvent(event);
+            this.svg.setPointerCapture(event.pointerId); // Captures pointer focus
             this.newViewBox.x = this.viewBox.x + (this.pointerOrigin.x - pointerPosition.x);
             this.newViewBox.y = this.viewBox.y + (this.pointerOrigin.y - pointerPosition.y);
             let rec = this.svg.getBoundingClientRect();
             this.svg.setAttribute("viewBox", `${this.newViewBox.x} ${this.newViewBox.y} ${rec.width} ${rec.height}`);
-        });
+        },{ passive: false });
         window.addEventListener('resize', (event) => {
             let rec = this.svg.getBoundingClientRect();
             this.svg.setAttribute("viewBox", `${this.newViewBox.x} ${this.newViewBox.y} ${rec.width} ${rec.height}`);
         });
     }
-    // This function returns an object with X & Y values from the pointer event
-    getPointFromEvent (event) {
-        var point = {x:0, y:0};
-        // If event is triggered by a touch event, we get the position of the first finger
-        if (event.targetTouches) {
-            point.x = event.targetTouches[0].clientX;
-            point.y = event.targetTouches[0].clientY;
-        } else {
-            point.x = event.clientX;
-            point.y = event.clientY;
-        }
-        
-        return point;
+
+    getPointFromEvent(event) {
+        return {
+            x: event.clientX,
+            y: event.clientY
+        };
     }
 
     tick(deltaTime){
